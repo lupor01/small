61f5a2a2a3f43ae1778f51f5a1f9f570438d2a60
lupor01 <rlupor01@student.bbk.ac>
2026-03-30 22:34:38 +0100

    basic API, get post key

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
 from fastapi import FastAPI, Header, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from transformers import pipeline
-import joblib
-import numpy as np
+import transformers 
+
  
 # print(fastapi.__version__)
 
-# classifier = pipeline("sentiment-analysis")
-# app = FastAPI()
+classifier = transformers.pipeline("sentiment-analysis",
+                      model="distilbert-base-uncased-finetuned-sst-2-english")
+app = FastAPI()
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_methods=["*"],
-#     allow_headers=["*"]
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
-# class TextInput(BaseModel): # make sure it matches!!!
-#     text: str # INPUT VALIDATION
+class TextInput(BaseModel): # make sure it matches!!!
+    text: str # INPUT VALIDATION
 
-# API_KEY = "segretissimo"
+API_KEY = "segretissimo"
 
-# def verify_key(api_key: str = Header(...)):
-#     if api_key != API_KEY:
-#         raise HTTPException(status_code=401, detail="API key not valid")
+def verify_key(api_key: str = Header(...)):
+    if api_key != API_KEY:
+        raise HTTPException(status_code=401, detail="API key not valid")
+    
+@app.get("/welcome")
+def welcome():
+    return {"message": "We are live!!"}
 
-# @app.post("/sentiment")
-# def analyse(data: TextInput, api_key: str = Depends(verify_key)) -> dict:
-#     result = classifier(data.text)[0] # predition here!!!
-#     return {
-#         "label": result["label"],
-#         "score": float(result["score"])
-#     }
+@app.post("/sentiment")
+def analyse(data: TextInput, api_key: str = Depends(verify_key)) -> dict:
+    result = classifier(data.text)[0] # predition here!!!
+    return {
+        "label": result["label"],
+        "score": float(result["score"])
+    }
