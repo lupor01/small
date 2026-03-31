07a6f96ef59ca71ff83fc213c0dedaad8433ae5a
lupor01 <rlupor01@student.bbk.ac>
2026-03-31 11:44:21 +0100

    added frontend

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from fastapi import FastAPI, Header, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 import transformers 
 
- 
-# print(fastapi.__version__)
 
-classifier = transformers.pipeline(
-    "sentiment-analysis",
-    model="distilbert-base-uncased-finetuned-sst-2-english"
-)
 app = FastAPI()
 
+
+# handling JS, CSS and HTML
+app.mount("/frontend", StaticFiles(directory="frontend"), name="frontend")
+html = Jinja2Templates(directory="frontend")
+
+@app.get("/")
+def home(request: Request):
+    return html.TemplateResponse("index.html", {"request": request})
+
+
+# escape localhost
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -19,18 +27,23 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+
+# nlp stuff
+classifier = transformers.pipeline(
+    "sentiment-analysis",
+    model="distilbert-base-uncased-finetuned-sst-2-english"
+)
 class TextInput(BaseModel): # make sure it matches!!!
     text: str # INPUT VALIDATION
 
+
+# DON'T HARDCODE IN ACTUAL DEPLOYMENT!
 API_KEY = "segretissimo"
 
 def verify_key(api_key: str = Header(...)):
     if api_key != API_KEY:
         raise HTTPException(status_code=401, detail="API key not valid")
     
-@app.get("/welcome")
-def welcome():
-    return {"message": "We are live!!"}
 
 @app.post("/sentiment")
 def analyse(data: TextInput, api_key: str = Depends(verify_key)) -> dict:
